8064ae4ce406d8fa51198ccabf884f04d759952b
Sonwabile Cwala <sonwabilecwala6@gmail.com>
2023-06-20 23:30:21 +0200

    finding Elements Using query Selector

---
+++ web-static/Form Input Attributes/Using the Form Action Input Attribute in HTML5/scripts/HR.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+    Thank you!<br>
+    Name: <?php echo $_GET["name"];?><br>
+    Last Name: <?php echo $_GET["lastName"];?><br>
+    Email: <?php echo $_GET["email"];?><br>
+    Cell: <?php echo $_GET["cell"];?><br>
+    </body>
+</html>
